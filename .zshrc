48bfdfbdbb7622f1e161600eb5a348a755b4dec5
Luis Collymoore <dedseq@luiss-mbp.myfiosgateway.com>
2022-10-01 15:24:13 -0400

    added features to add & delete and error checking when fields are missing

---
+++ .zshrc
@@ -0,0 +1,2 @@
+export NVM_DIR=~/.config/nvm
+source $(brew --prefix nvm)/nvm.sh
--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const http = require('http')
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
+const PORT = 3001
+const morgan = require('morgan')
+app.use(express.json())
+app.use(morgan('tiny'))
+
 
 let persons = [
 	{ 
@@ -12,7 +18,7 @@ let persons = [
       "id": 2,
       "name": "Ada Lovelace", 
       "number": "39-44-5323523"
-    },
+    }, 
     { 
       "id": 3,
       "name": "Dan Abramov", 
@@ -34,11 +40,62 @@ app.get('/api/persons', (request, response)=>{
 })
 
 app.get('/info', (request, response) =>{
-	
-
-	
+const currentDate = new Date()
+response.send(`<h2>Phonebook has info for ${persons.length} 
+people</h2> <h2>${currentDate}</h2>`)
+ 
 })
 
-const PORT = 3001
-app.listen(PORT)
+app.get('/api/persons/:id', (request, response)=>{
+const id = request.params.id;
+const entry = persons.find(entry => entry.id == id)
+if (entry) {
+response.json(entry)
+}else{
+response.status(404).end()
+}
+})
+
+app.delete('/api/persons/:id', (request, response) =>{
+  const id = Number(request.params.id)
+  persons = persons.filter(entry => entry.id !== id)
+  response.status(204).end()
+
+})
+
+
+const generateId = () => {
+  const maxId = persons.length > 0
+    ? Math.max(...persons.map(n => n.id))
+    : 0
+  return maxId + 1
+}
+app.post('/api/persons', (request, response)=>{
+  const body = request.body
+  
+  if (!body.name) {
+    return response.status(400).json({ 
+      error: 'name missing' 
+    })
+  }
+  if(!body.number){
+    //custom error message for missing number
+    return response.status(400).json({error: 'number missing'})
+  }
+  if(persons.some(entry => entry.name === body.name)){
+   //return error message if name already exists
+    return response.status(409).json({error: 'name must be unique'})
+  }
+
+  let entry = {
+    id: generateId(),
+    name: body.name,
+    number: body.number
+  }
+  persons.push(entry)
+  response.json(entry)
+
+})
+app.listen(PORT, () => {
 console.log(`Server running on port ${PORT}`)
+})
